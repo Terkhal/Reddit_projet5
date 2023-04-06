9e3b200f7944371108203d8034a2920c3fa675e9
Lorie-Oh <124519686+Lorie-Oh@users.noreply.github.com>
2023-04-06 14:14:02 +0200

    comp top cat

---
+++ Brag-it/src/components/Comp_home_topCateg.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+let ID = ref([])
+const url = 'https://jsonplaceholder.typicode.com/comments?_limit=10'
+
+onMounted(() => {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            let tmp = data.map(comments => comments.postId)
+            //   comments.value = data
+            let result = {}
+            tmp.forEach(function (x) { result[x] = (result[x] || 0) + 1; });
+            ID.value = result
+            console.log(ID)
+        })
+        // eslint-disable-next-line no-unused-vars
+        .catch((error) => {
+            console.log(error)
+            console.error('oulaaah erreur')
+        })
+})
+
+console.log();
+
+Math.max(1,2){
+
+}
+function topPost (ID)
+
+</script>
+
+<template>
+    <div class="categ">
+
+        <table class="topcat">
+            <p> test ci dessous</p>
+            <div v-if="ID.value != undefined">
+                <div v-for="comment in comments.postId" :key="comment.postId">
+                </div>
+            </div>
+
+            <tr>
+                <th> &#128293; POPULAR </th>
+            </tr>
+            <tr>
+                <td> TOP VALUE {{ ID }} </td>
+            </tr>
+            <tr>
+                <td> categ 2 </td>
+            </tr>
+            <tr>
+                <td> categ 3 </td>
+            </tr>
+        </table>
+    </div>
+
+</template>
+
+<style>
+th,
+td {
+    border-color: #020202;
+}
+
+/* 
+.categ{
+    float:left;
+} */
+
+/* .topcat{
+    background-color: blueviolet;
+ }  */
+</style>
--- Brag-it/src/views/View_home.vue
+++ Brag-it/src/views/View_home.vue
@@ -1,7 +1,20 @@
 <script setup>
+import Comp_topCat from '../components/Comp_home_topCateg.vue';
+
 </script>
 
 <template>
-  <main>
-  </main>
+<!-- include nav barre -->
+<!-- include top cat -->
+<!-- include add post -->
+<!-- include post liste -->
+<div class="categ">
+  <Comp_topCat/>
+ </div>
 </template>
+
+<style> 
+.categ{
+  float: left;
+}
+</style>
