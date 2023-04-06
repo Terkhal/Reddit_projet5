2572c57aa115762f8d9dcbe7bce056f1043c00eb
leo <leo.calone@epitech.eu>
2023-04-06 14:19:38 +0200

    leo

--- Brag-it/src/components/Comp_newPost_form.vue
+++ Brag-it/src/components/Comp_newPost_form.vue
@@ -1,3 +1,11 @@
 <script setup>
 
+const body = {
+    
+}
+const requestOption = {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({})
+}
 </script>
