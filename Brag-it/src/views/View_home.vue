d547df02d1212d2e35b3f298f8f63eddd330f448
Lorie-Oh <124519686+Lorie-Oh@users.noreply.github.com>
2023-04-06 15:21:16 +0200

    dernieres modifs avant pull

--- Brag-it/src/views/View_home.vue
+++ Brag-it/src/views/View_home.vue
@@ -1,18 +1,26 @@
 <script setup>
-// import Comp_home_postsLists from '../components/Comp_home_postsLists.vue';
+import Comp_home_postsLists from '../components/Comp_home_postsLists.vue';
+
+</script>
+
+<template>
+  
+    <Comp_home_postsLists/>
+
+</template>
+<script setup>
 import Comp_topCat from '../components/Comp_home_topCateg.vue';
 
 </script>
 
 <template>
-  <!-- include nav barre -->
-  <!-- include top cat -->
-  <!-- include add post -->
-  <!-- include post liste -->
-  <!-- <div class="categ"> -->
-    <Comp_topCat/>
-    <!-- <Comp_home_postsLists/> -->
- <!-- </div> -->
+<!-- include nav barre -->
+<!-- include top cat -->
+<!-- include add post -->
+<!-- include post liste -->
+<div class="categ">
+  <Comp_topCat/>
+ </div>
 </template>
 
 <style> 
