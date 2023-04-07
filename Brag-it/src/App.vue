49fb289b29c1da6cf7ee4b3277a4b96789253d8e
arthur <arthur.asselin@epitech.eu>
2023-04-07 12:35:57 +0200

    removed temp file

--- Brag-it/src/App.vue
+++ Brag-it/src/App.vue
@@ -1,6 +1,22 @@
 <script setup>
 import View_home from './views/View_home.vue';
 import ThemeButton from "@/components/ThemeButton.vue";
+import countcom from './components/Comp_home_topCateg_Count.vue'
+
+// test api
+
+import { ref } from 'vue';
+
+const categ = ref([]);
+const posts = ref([]);
+
+fetch('http://127.0.0.1:8001/api/categories')
+.then((response) => response.json() )
+.then((data) => {console.log(data)
+categ.value = data} )
+
+
+
 
 </script>
 
@@ -14,13 +30,11 @@ import ThemeButton from "@/components/ThemeButton.vue";
 
 <body>
   <div class="card">
-      <h1>Popular</h1>
-      <p>Jordan</p>
-      <p>Car</p>
-      <p>Pigeon</p>
+    <div>{{ categ.length }}</div>
+    <div v-for="cat in categ">{{ cat.name }}  <countcom :catid="cat.id"/></div>
   </div>
 
-  <View_home/>
+  <!-- <View_home/> -->
 </body>
 
 </div>
