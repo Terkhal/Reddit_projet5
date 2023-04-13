abb693b75a5b657cbd4c21382ad3c206fde36363
JinClaudius <jeanbart406@gmail.com>
2023-04-13 16:32:26 +0200

    push datacontroller

--- brag_it_sanctum/app/Http/Controllers/Api/DataController.php
+++ brag_it_sanctum/app/Http/Controllers/Api/DataController.php
@@ -4,22 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Posts;
-use App\Models\Categories;
-use App\Models\Comments;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Support\Facades\Validator;
 
 class DataController extends Controller
 {
-    public function topCat()
+    public function alldatacount()
     {
 
+        $usersCount = DB::table('users')->count();
+        $adminUsersCount = DB::table('users')->where('is_admin', 1)->count();
+        $categoriesCount = DB::table('categories')->count();
+        $postsCount = DB::table('posts')->count();
+        $commentsCount = DB::table('comments')->count();
 
-        return Comments::groupBy('category_id')
-            ->selectRaw('count(*) as total, category_id')
-            ->with("category")
-            ->get();
-        // On retourne les informations des utilisateurs en JSON
+        $counts = [
+            'users' => $usersCount,
+            'admin_users' => $adminUsersCount,
+            'categories' => $categoriesCount,
+            'posts' => $postsCount,
+            'comments' => $commentsCount,
+        ];
+
+        return response()->json($counts);
     }
 }
