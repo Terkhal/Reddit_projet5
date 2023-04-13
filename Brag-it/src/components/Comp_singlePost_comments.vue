bbfd1da4661a9b5f4a9778b4507c0e999ce6959f
leo <leo.calone@epitech.eu>
2023-04-13 10:08:23 +0200

    leo

---
+++ Brag-it/src/components/Comp_singlePost_comments.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref} from "vue"
+import {useRoute} from "vue-router"
+import Comp_singlepost_form_comments from "./Comp_singlepost_form_comments.vue";
+
+let comments = ref([""])
+
+let route = useRoute()
+let postId = route.params.post_id;
+let url = 'http://127.0.0.1:8000/api/comments/post/';
+
+
+
+fetch(url + postId)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        comments.value = data;
+        // console.log(comments.content);
+    })
+
+</script>
+
+<template>
+    {{ contents }}
+    <div class="comment" v-for="comment in comments"  :key="comment.id">
+        <comment> {{ comment.content }} </comment>
+        <comment> {{ comment.id }} </comment>
+    </div>
+    <Comp_singlepost_form_comments :Id="postId"/>
+</template>     
